27e6e59f92179978b38a7a8e5170b03534d3fe87
Adam Hupp <adam@hupp.org>
2019-12-06 20:37:26 -0800

    day5 working

--- aoc2019/day5/src/main.rs
+++ aoc2019/day5/src/main.rs
@@ -2,66 +2,135 @@ use std::io::{BufRead, self, BufReader};
 use std::env;
 use std::fs::File;
 
-enum Value {
-    Lookup(i32),
-    Immediate(i32),
+struct Prog {
+    mem: Vec<i32>,
+    ip: usize,
+    modes: (bool, bool, bool),
+    op: i32,
 }
 
-enum Inst {
-    Unary(Value),
-    Binary(Value, Value, Value),    
-}
+impl Prog {
+    fn new(mem : Vec<i32>) -> Prog {
+        let mut p = Prog {mem: mem, ip: 0, modes: (false, false, false), op: 0};
+        p.set_ip(0);
+        p
+    }
 
-fn lookup(mem: &Vec<i32>, pos: usize, immediate: bool) -> i32 {
-    if immediate {
-        mem[pos]
-    } else {
-        mem[mem[pos] as usize]
+    fn set_ip(&mut self, pos: usize) {
+        self.ip = pos;
+        let full_op = self.mem[self.ip];
+        self.op = full_op % 100;
+        let a_mode = ((full_op / 100) % 2) > 0;
+        let b_mode = ((full_op / 1000) % 2) > 0;
+        let c_mode = ((full_op / 10000) % 2) > 0;
+        self.modes = (a_mode, b_mode, c_mode);
+    }
+
+    fn write(&mut self, idx: usize, value: i32) {
+        let dest = self.mem[self.ip + idx] as usize;
+        //println!("write {} {} {}", idx, dest, value);
+        self.mem[dest] = value;
+    }
+
+    fn param(&self, i: usize) -> i32 {
+        let pos = self.ip + i;
+        let is_immediate = match i {
+            1 => self.modes.0,
+            2 => self.modes.1,
+            3 => self.modes.2,
+            _ => panic!("bad param index {}", i),
+        };
+        if is_immediate {
+            self.mem[pos]
+        } else {
+            self.mem[self.mem[pos] as usize]
+        }
+        //println!(" param {} {} {}", i, a, is_immediate);
+    }
+
+    fn eval(&mut self) -> usize {
+        //println!("op {}", self.op);
+        match self.op {
+            1 => self.eval_bin(|x,y| x + y),
+            2 => self.eval_bin(|x,y| x * y),
+            3 => self.eval_store(),
+            4 => self.eval_out(),
+            5 => self.eval_jump_if_true(),
+            6 => self.eval_jump_if_false(),
+            7 => self.eval_lt(),
+            8 => self.eval_eq(),
+            99 => self.mem.len(),
+            _ => panic!("unknown opcode {}", self.op),
+        }
+    
+    }
+
+    fn run(&mut self) {
+        loop {
+            let ip = self.eval();
+            if ip == self.mem.len() {
+                break;
+            }
+            self.set_ip(ip);
+        }
+    }
+    
+    fn eval_bin<F : Fn(i32,i32)->i32>(&mut self, fun: F) -> usize {
+        let ret = fun(self.param(1), self.param(2));
+        self.write(3, ret);
+        self.ip + 4
+    }
+    
+    fn eval_store(&mut self) -> usize {
+        let mut buffer = String::new();
+        println!("input: ");
+        io::stdin().read_line(&mut buffer).unwrap();
+        let val : i32 = buffer.trim_end().parse().unwrap();
+        self.write(1, val);
+        self.ip + 2
+    }
+    
+    fn eval_out(&mut self) -> usize {
+        let val = self.param(1);
+        println!("out: {}", val);
+        self.ip + 2
+    }    
+
+    fn eval_jump_if_true(&mut self) -> usize {
+        let v = self.param(1);
+        if v > 0 {
+            self.param(2) as usize
+        } else {
+            self.ip + 3
+        }
+    }
+
+    fn eval_jump_if_false(&mut self) -> usize {
+        let v = self.param(1);
+        if v == 0 {
+            self.param(2) as usize
+        } else {
+            self.ip + 3
+        }
+    }
+
+    fn eval_lt(&mut self) -> usize {
+        let a = self.param(1);
+        let b = self.param(2);
+        self.write(3, if a < b { 1 } else { 0 });
+        self.ip + 4
+    }
+
+    fn eval_eq(&mut self) -> usize {
+        let a = self.param(1);
+        let b = self.param(2);
+        self.write(3, if a == b { 1 } else { 0 });
+        self.ip + 4
     }
 }
 
-fn eval_bin<F : Fn(i32,i32)->i32>(mem : &mut Vec<i32>, pos : usize, fun : F, a_mode: bool, b_mode: bool) -> usize {
-    let ret = fun(lookup(mem, pos, a_mode), lookup(mem, pos+1, b_mode));
-    let dest = mem[pos+2] as usize;
-    mem[dest] = ret;
-    pos + 3
-}
 
-fn eval_store(mem: &mut Vec<i32>, pos : usize) -> usize {
-    let mut buffer = String::new();
-    println!("input: ");
-    io::stdin().read_line(&mut buffer).unwrap();
-    let val : i32 = buffer.trim_end().parse().unwrap();
-    let dest = mem[pos] as usize;
-    mem[dest] = val;
-    pos + 1
-}
-
-fn eval_out(mem: &mut Vec<i32>, pos : usize) -> usize {
-    let val = mem[mem[pos] as usize];
-    println!("out: {}", val);
-    pos + 1
-}
-
-// Evaluate the instruction at `pos`, and return the position of the next
-// instruction
-fn eval_at(mem : &mut Vec<i32>, pos : usize) -> usize {
-    let full_op = mem[pos];
-    let op = full_op % 100;
-    let a_mode = (full_op / 100 % 2) > 0;
-    let b_mode = (full_op / 1000 % 2) > 0;
-    let c_mode = (full_op / 10000 % 2) > 0;
-    match op {
-        1 => eval_bin(mem, pos+1, |x,y| x + y, a_mode, b_mode),
-        2 => eval_bin(mem, pos+1, |x,y| x * y, a_mode, b_mode),
-        3 => eval_store(mem, pos+1),
-        4 => eval_out(mem, pos + 1),
-        99 => mem.len(),
-        _ => panic!("unknown opcode {}", mem[pos]),
-    }
-}
-
-fn read_prog(reader : &mut BufReader<File>) -> Vec<i32> {
+fn read_prog(reader : &mut BufReader<File>) -> Prog {
     let mut mem : Vec<i32> = Vec::new();
     for line in reader.lines() {
         for entry in line.unwrap().split(",") {
@@ -69,14 +138,7 @@ fn read_prog(reader : &mut BufReader<File>) -> Vec<i32> {
             mem.push(as_int);
         }
     }
-    mem
-}
-
-fn run_prog(mut mem : &mut Vec<i32>) {
-    let mut ip = 0;
-    while ip < mem.len() {
-        ip = eval_at(&mut mem, ip);
-    }
+    Prog::new(mem)
 }
 
 fn main() {
@@ -85,5 +147,5 @@ fn main() {
     let file = File::open(program).unwrap();
     let mut reader = BufReader::new(file);
     let mut prog = read_prog(&mut reader);
-    run_prog(&mut prog);
+    prog.run();
 }
